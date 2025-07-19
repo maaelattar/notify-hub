095fa6f228f820f5f36e482e5c43e1694d8e444b
Mohamed Elattar <maaelattar@gmail.com>
2025-07-19 18:59:53 +0300

    NTH-003: Module Architecture Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NotificationsModule } from './modules/notifications/notifications.module';
+import { ChannelsModule } from './modules/channels/channels.module';
+import { SharedModule } from './modules/shared/shared.module';
 
 @Module({
-  imports: [],
+  imports: [
+    SharedModule,
+    NotificationsModule,
+    ChannelsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
